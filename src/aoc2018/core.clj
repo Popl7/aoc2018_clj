ac300ac9bae9ebb461541624f6a599bbd559cf27
Steven Thonus <steven.thonus@infi.nl>
2018-12-02 17:14:33 +0100

    finally managed day2 part2 :-)

--- src/aoc2018/core.clj
+++ src/aoc2018/core.clj
@@ -1,6 +1,6 @@
 (ns aoc2018.core
   (:gen-class)
-  (:require [aoc2018.day1 :refer [part2]]))
+  (:require [aoc2018.day2 :refer [part2]]))
 
 (defn -main
   "I don't do a whole lot ... yet."
--- src/aoc2018/day2.clj
+++ src/aoc2018/day2.clj
@@ -1,6 +1,5 @@
 (ns aoc2018.day2
-  (:require [clojure.set :refer [map-invert]]))
-
+  (:require [clojure.set :refer [map-invert difference]]))
 
 (def lines
   (->>
@@ -9,7 +8,15 @@
    (slurp)
    (clojure.string/split-lines)))
 
-;; (def lines ["aabcdd" "aabddd"])
+;;(def lines ["aabcdd" "aabddd"])
+;; (def lines ["abcde"
+;;              "fghij"
+;;              "klmno"
+;;              "pqrst"
+;;              "fguij"
+;;              "axcye"
+;;             "wvxyz"
+;;             "fgaaa"])
 
 (defn map-inverse [m]
   (reduce (fn [m' [k v]] (update m' v conj k)) {} m))
@@ -19,9 +26,31 @@
         freq (map-inverse (frequencies letters))]
     (count (get freq amount))))
 
-(let [twos (map #(count-id % 2) lines)
-      threes (map #(count-id % 3) lines)
-      total-twos (count (filter #(< 0 %) twos))
-      total-threes (count (filter #(< 0 %) threes))
-      checksum (* total-twos total-threes)]
-  (println "checksum " total-twos " * " total-threes " = " checksum))
+(defn part1 []
+  (let [twos (map #(count-id % 2) lines)
+        threes (map #(count-id % 3) lines)
+        total-twos (count (filter #(< 0 %) twos))
+        total-threes (count (filter #(< 0 %) threes))
+        checksum (* total-twos total-threes)]
+    (println "checksum " total-twos " * " total-threes " = " checksum)))
+
+
+
+(defn stevenshtein [str1 str2]
+  (let [seq1  (seq (char-array str1))
+        seq2  (seq (char-array str2))
+        same (map-indexed #(if (= %2 (nth seq1 %1))
+                             %2) seq2)
+        same (remove nil? same)]
+    (if (= (count same) (dec (count str1)))
+      same)))
+
+(defn part2 []
+  (let [similars (map (fn [l]
+                        (let [similar (map #(stevenshtein l %) lines)
+                              similar (remove nil? similar)]
+                          [l similar]))
+                      lines)
+        matched (filter (fn [[k v]] (seq v)) similars)
+        matched (map (fn [[k v]] v) matched)]
+    (println "matched" (apply str (first (first matched))))))
